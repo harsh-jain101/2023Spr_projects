63f456f7f2f195c27d27113d3c5af686f1714242
MandarParkar <mandarparkar99@gmail.com>
2023-04-30 21:17:50 -0500

    added doctests in cleaning.py

--- cleaning.py
+++ cleaning.py
@@ -6,14 +6,23 @@ import json
 import re
 import seaborn as sns
 import folium
+
+
 def detect_number(x: str) -> bool:
     """
     Determines whether a given string contains a numerical value or not.
 
     :param x: str: string to check for numerical value
     :return: bool: True if numerical value is present, False otherwise
+
+    >>> detect_number('234')
+    True
+    >>> detect_number('sdf 423 sf 4')
+    True
+    >>> detect_number('ter eev sdf')
+    False
     """
-    pattern = re.compile('^.*\d.*$')
+    pattern = re.compile(r'^.*\d.*$')
     if re.match(pattern, x) is None:
         return False
     return True
@@ -25,18 +34,29 @@ def find_salary(salary_string: str) -> list[float]:
 
     :param salary_string: str: string containing salary information
     :return: List[float]: list of extracted salaries
+
+    >>> find_salary('afdslk')
+    []
+    >>> find_salary('$356-$235')
+    [356.0, 235.0]
+    >>> find_salary('235k-45k')
+    [235000.0, 45000.0]
+    >>> find_salary('$456M-678m')
+    [456000000.0, 678000000.0]
+    >>> find_salary('$456.67-678.56')
+    [456.67, 678.56]
     """
     salary_string = salary_string.lower().replace(',', '')
-    pattern = re.compile('[\d(\.\d)?]+')
+    pattern = re.compile(r'[\d(\.\d)?]+')
     salaries = re.findall(pattern, salary_string)
     for idx in range(len(salaries)):
         check_idx = salary_string.index(salaries[idx]) + len(salaries[idx])
         if check_idx == len(salary_string) or not (salary_string[check_idx] in ['k', 'm']):
-            salaries[idx] = float(salaries[idx])
+            salaries[idx] = round(float(salaries[idx]), 2)
         elif salary_string[check_idx] == 'k':  # For example - 100k meaning 100000 (k=1000)
-            salaries[idx] = float(salaries[idx]) * 1000
+            salaries[idx] = round(float(salaries[idx]) * 1000, 2)
         elif salary_string[check_idx] == 'm':  # For example 1m meaning 1000000 (m= million)
-            salaries[idx] = float(salaries[idx]) * math.pow(10, 6)
+            salaries[idx] = round(float(salaries[idx]) * math.pow(10, 6), 2)
 
     return salaries
 
@@ -48,6 +68,19 @@ def determine_payment_frequency(salary_string: str, salaries: list[float]) -> st
     :param salary_string: str: string containing salary information
     :param salaries: List[float]: list of extracted salaries
     :return: str: payment frequency (hourly, monthly, weekly or yearly)
+
+    >>> determine_payment_frequency('$345 hr', [345.00])
+    'hourly'
+    >>> determine_payment_frequency('$345k-$450k annual', [345000.00, 450000.00])
+    'yearly'
+    >>> determine_payment_frequency('$345', [345.00])
+    'hourly'
+    >>> determine_payment_frequency('$345 a week', [345.00])
+    'weekly'
+    >>> determine_payment_frequency('$345/mo', [345.00])
+    'monthly'
+    >>> determine_payment_frequency('$50k', [50000.00])
+    'yearly'
     """
     frequency = None
     hourly = ['hr', 'hourly', 'hour']
@@ -80,6 +113,13 @@ def det_salary_range_and_frequency(salary_string: str) -> dict:
 
     :param salary_string: str: string containing salary data
     :return dictionary containing 'min_salary', 'max_salary', and 'frequency' as keys
+
+    >>> det_salary_range_and_frequency('$345k-$450k annual')
+    {'min_salary': 345000.0, 'max_salary': 450000.0, 'frequency': 'yearly'}
+    >>> det_salary_range_and_frequency('$345-$450 per week')
+    {'min_salary': 345.0, 'max_salary': 450.0, 'frequency': 'weekly'}
+    >>> det_salary_range_and_frequency('$15-$20')
+    {'min_salary': 15.0, 'max_salary': 20.0, 'frequency': 'hourly'}
     """
     salaries = find_salary(salary_string)
     if len(salaries) == 1:
@@ -123,3 +163,7 @@ def calc_max_comp(row: pd.Series) -> float:
     :return: float: calculated maximum annual compensation
     """
     return calculate_annual_compensation(row, 'max')
+
+
+if __name__=='__main__':
+    pass
